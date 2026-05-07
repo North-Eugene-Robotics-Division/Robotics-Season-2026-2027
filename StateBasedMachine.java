e9ea18af5236684ffbad53bac1de62f0ed3c07c8
Rhone Fleming <151577884+rfleming01@users.noreply.github.com>
2026-05-06 19:22:14 -0700

    Update StateBasedMachine.java
    Added construct script
    Filled AddSBM function

--- StateBasedMachine.java
+++ StateBasedMachine.java
@@ -9,8 +9,10 @@ import java.util.Collections;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.teamcode.Hardware;
 
-public class StateBasedMachine {
+public class SBM {
 
 	// HardwareDevice to allow for servos and motors, both implement it.
 	private ArrayList<HardwareDevice> objects = new ArrayList<HardwareDevice>();
@@ -20,10 +22,18 @@ public class StateBasedMachine {
 	
 	//ResetState is the value to set the hardware device to after the time is up - usually a default servo position or 0 for motors
 	private ArrayList<Float> resetState = new ArrayList<Float>();
+	
+	private SBMTestHardware theRobot = null;
+	
+	public SBM(SBMTestHardware hardware) {
+		theRobot = hardware;
+	}
 
 	//Used by other files to add objects and their timers to the SBM checklist
-	public void AddSBM(HardwareDevice object, double length, double runtime, String Type, float State) {
-		
+	public void AddSBM(HardwareDevice object, double length, double runtime, float state) {
+		objects.add(object);
+		finishTime.add(runtime+length);
+		resetState.add(state);
 	}
 	
 	public void checkSBM(double runtime) {
@@ -35,8 +45,14 @@ public class StateBasedMachine {
 				if (objects.get(i) instanceof DcMotor) {
 					DcMotor temp = (DcMotor) objects.get(i);
 					temp.setPower(resetState.get(i));
+				} else if (objects.get(i) instanceof Servo) {
+					Servo temp = (Servo) objects.get(i);
+					temp.setPosition(resetState.get(i));
+				} else {
+					
 				}
-			}
+				
+			} 
 		}
 	}
 }
