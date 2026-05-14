794c9ea425aaaa8851cf476281131edabb41a865
Rhone Fleming <151577884+rfleming01@users.noreply.github.com>
2026-05-14 08:33:42 -0700

    Update SBMDriveCode.java
    Uses new SBM constructor instead of just the hardware file
    Now outputs various telemetry.

--- StateBasedMachineTesting/SBMDriveCode.java
+++ StateBasedMachineTesting/SBMDriveCode.java
@@ -13,12 +13,15 @@ import org.firstinspires.ftc.teamcode.SBMTestHardware;
 public class SBMTestDriveCode extends LinearOpMode {
 	private ElapsedTime runtime = new ElapsedTime();
 	
+	private int trackCheckTimeRuns = 0;
+	
 	private double checkTime;
 
 	@Override
 	public void runOpMode() {
 		
 		SBMTestHardware robot = new SBMTestHardware(this);
+		SBM thisMachine = new SBM(this);
 		robot.init();
 		
 		telemetry.addData("Status", "Initialized");
@@ -31,6 +34,11 @@ public class SBMTestDriveCode extends LinearOpMode {
 		// run until the end of the match (driver presses STOP)
 		while (opModeIsActive()) {
 			telemetry.addData("Status", "Running");
+			telemetry.addData("Runtime: ", runtime.milliseconds());
+			telemetry.addData("SBM Checks: ", trackCheckTimeRuns);
+			telemetry.addData("Tracked Objects; ",thisMachine.SBMTelemetry("Objects"));
+			telemetry.addData("Tracked End Times: ",thisMachine.SBMTelemetry("FinishTime"));
+			telemetry.addData("Tracked Reset States: ",thisMachine.SBMTelemetry("ResetState"));
 			telemetry.update();
 			
 			if(gamepad1.xWasReleased()){
@@ -39,8 +47,9 @@ public class SBMTestDriveCode extends LinearOpMode {
 			if(gamepad1.yWasReleased()){
 				robot.runMotor2(runtime.milliseconds());
 			}
-			if(checkTime>runtime.milliseconds()){
-				robot.runCheckSBM(runtime.milliseconds());
+			if(checkTime<runtime.milliseconds()){
+				thisMachine.checkSBM(runtime.milliseconds());
+				trackCheckTimeRuns++;
 			}
 		}
 	}
