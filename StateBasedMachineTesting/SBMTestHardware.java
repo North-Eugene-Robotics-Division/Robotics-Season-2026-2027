5c4e6db284a09536e7fa1ed5bcdb3a375436b11c
Rhone Fleming <151577884+rfleming01@users.noreply.github.com>
2026-05-14 08:32:38 -0700

    Update SBMTestHardware.java

--- StateBasedMachineTesting/SBMTestHardware.java
+++ StateBasedMachineTesting/SBMTestHardware.java
@@ -21,11 +21,11 @@ public class SBMTestHardware {
 	public DcMotor motor1 = null;
 	public DcMotor motor2 = null;
 	
-	public Servo servo1 = null;
-	public Servo servo2 = null;
+	// public Servo servo1 = null;
+	// public Servo servo2 = null;
 	
-	public CRServo crServo1 = null;
-	public CRServo crServo2 = null;
+	// public CRServo crServo1 = null;
+	// public CRServo crServo2 = null;
 	
 	private SBM thisMachine = null;
 	
@@ -37,17 +37,17 @@ public class SBMTestHardware {
 		motor1 = myOpMode.hardwareMap.get(DcMotor.class, "Motor1");
 		motor2 = myOpMode.hardwareMap.get(DcMotor.class, "Motor2");
 		
-		servo1 = myOpMode.hardwareMap.get(Servo.class, "Servo1");
-		servo2 = myOpMode.hardwareMap.get(Servo.class, "Servo2");
+		// servo1 = myOpMode.hardwareMap.get(Servo.class, "Servo1");
+		// servo2 = myOpMode.hardwareMap.get(Servo.class, "Servo2");
 		
-		crServo1 = myOpMode.hardwareMap.get(CRServo.class, "crServo1");
-		crServo2 = myOpMode.hardwareMap.get(CRServo.class, "crServo2");
+		// crServo1 = myOpMode.hardwareMap.get(CRServo.class, "crServo1");
+		// crServo2 = myOpMode.hardwareMap.get(CRServo.class, "crServo2");
 		
 		motor1.setDirection(DcMotor.Direction.FORWARD);
 		motor2.setDirection(DcMotor.Direction.FORWARD);
 	
-		servo1.setPosition(0);
-		servo2.setPosition(0);
+		// servo1.setPosition(0);
+		// servo2.setPosition(0);
 		
 		thisMachine = new SBM(this);
 	}
@@ -55,12 +55,12 @@ public class SBMTestHardware {
 	public void runMotor1(double runtime) {
 		motor1.setPower(.5);
 		
-		thisMachine.AddSBM(motor1, 3000, runtime, 0);
+		thisMachine.addSBM(motor1, 3000, runtime, 0);
 	}
 	public void runMotor2(double runtime) {
 		motor2.setPower(.3);
 		
-		thisMachine.AddSBM(motor1, 4000, runtime, 0);
+		thisMachine.addSBM(motor1, 4000, runtime, 0);
 	}
 	public void runCheckSBM(double runtime){
 		thisMachine.checkSBM(runtime);
