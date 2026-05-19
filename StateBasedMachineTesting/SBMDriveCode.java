5c7cd5b432711b8f3dc248169ff17ab22cb22081
Rhone Fleming <151577884+rfleming01@users.noreply.github.com>
2026-05-19 15:57:59 -0700

    Update SBMDriveCode.java

--- StateBasedMachineTesting/SBMDriveCode.java
+++ StateBasedMachineTesting/SBMDriveCode.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.robot.Robot;
+import java.util.ArrayList;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
 
 import org.firstinspires.ftc.teamcode.SBMTestHardware;
 
@@ -14,6 +16,7 @@ public class SBMTestDriveCode extends LinearOpMode {
 	private ElapsedTime runtime = new ElapsedTime();
 	
 	private int trackCheckTimeRuns = 0;
+	private int cycleTime = 1000;
 	
 	private double checkTime;
 
@@ -24,8 +27,8 @@ public class SBMTestDriveCode extends LinearOpMode {
 		SBM thisMachine = new SBM(this);
 		robot.init();
 		
-		telemetry.addData("Status", "Initialized");
-		telemetry.update();
+		// telemetry.addData("Status", "Initialized");
+		// telemetry.update();
 		// Wait for the game to start (driver presses PLAY)
 		waitForStart();
 		runtime.reset();
@@ -33,12 +36,21 @@ public class SBMTestDriveCode extends LinearOpMode {
 		
 		// run until the end of the match (driver presses STOP)
 		while (opModeIsActive()) {
-			telemetry.addData("Status", "Running");
+			// telemetry.addData("Status", "Running");
 			telemetry.addData("Runtime: ", runtime.milliseconds());
 			telemetry.addData("SBM Checks: ", trackCheckTimeRuns);
-			telemetry.addData("Tracked Objects; ",thisMachine.SBMTelemetry("Objects"));
-			telemetry.addData("Tracked End Times: ",thisMachine.SBMTelemetry("FinishTime"));
-			telemetry.addData("Tracked Reset States: ",thisMachine.SBMTelemetry("ResetState"));
+			// telemetry.addData("Tracked Objects; ",thisMachine.SBMTelemetry("Objects"));
+			// telemetry.addData("Tracked End Times: ",thisMachine.SBMTelemetry("FinishTime"));
+			// telemetry.addData("Tracked Reset States: ",thisMachine.SBMTelemetry("ResetState"));
+			// telemetry.update();
+
+			// ArrayList<HardwareDevice> objs = thisMachine.SBMTelemetry("Objects");
+			// ArrayList<Double> fTimes = thisMachine.SBMTelemetry("FinishTime");
+			
+			// for (int ObjNum = 0; ObjNum < objs.size(); ObjNum++){
+			// 	telemetry.addData("Object: ", "%.0f Until: %.2f", ObjNum, fTimes.get(ObjNum));
+			// 	telemetry.addData("Cycle", ObjNum);
+			// }
 			telemetry.update();
 			
 			if(gamepad1.xWasReleased()){
@@ -47,9 +59,10 @@ public class SBMTestDriveCode extends LinearOpMode {
 			if(gamepad1.yWasReleased()){
 				robot.runMotor2(runtime.milliseconds());
 			}
-			if(checkTime<runtime.milliseconds()){
+			if(checkTime+cycleTime<runtime.milliseconds()){
 				thisMachine.checkSBM(runtime.milliseconds());
 				trackCheckTimeRuns++;
+				checkTime = runtime.milliseconds();
 			}
 		}
 	}
